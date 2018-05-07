07e5c798549f8223bc53a9816c0fe875e29b2ba7
Glenn Ji <qiang.ji@yahoo.com.au>
2018-05-07 19:34:15 +1000

    Add variable_bindings

---
+++ src/bin/variable_bindings.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let an_integer = 2u32;
+    let copied_integer = an_integer;
+    println!("{}", an_integer);
+    println!("{}", copied_integer);
+    let _unused_variable: u32 = 2;
+}
