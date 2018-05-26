c41cac187189b7c4fcd4489eb7ce09be3b08327b
Qiang Ji <qiang.j@gmail.com>
2018-05-26 15:23:36 +1000

    Add struct generic

--- src/bin/generic.rs
+++ src/bin/generic.rs
@@ -8,8 +8,23 @@ fn largest(list: &[i32]) -> i32 {
     largest
 }
 
+#[derive(Debug)]
+struct Point<T, U> {
+    x: T,
+    y: U
+}
+
 fn main() {
     let list = vec![3, 2, 4, 6, 1];
     let largest = largest(&list);
     println!("{}", largest);
+
+    let p1 = Point {x: 1, y: 2};
+    println!("{:?}", p1);
+
+    let p2 = Point {x: 1.0, y: 2.0};
+    println!("{:?}", p2);
+
+    let p3 = Point {x: 1, y: 2.0};
+    println!("{:?}", p3);
 }
