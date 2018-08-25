cbde8dd97168add09eabe5ce6512a4c47b5787a1
Qiang Ji <qiang.j@gmail.com>
2018-08-25 18:02:59 +1000

    Implement custom Display for struct

--- src/bin/formatted_print.rs
+++ src/bin/formatted_print.rs
@@ -1,3 +1,13 @@
+use std::fmt;
+
+struct Rectangle(i32, i32);
+
+impl fmt::Display for Rectangle {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({}, {})", self.0, self.1)
+    }
+}
+
 fn main() {
     println!("{}", 32);
     println!("{1} {0}", 2, 3);
@@ -13,4 +23,6 @@ fn main() {
     let pi = 3.1415926;
     println!("{:.*}", 3, pi);
     println!("{:.3}", pi);
+
+    println!("{}", Rectangle(3, 4));
 }
