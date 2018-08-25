0345ecb73470a7c9d2d0273aa7da8aafeda9fe1c
Qiang Ji <qiang.j@gmail.com>
2018-08-25 18:36:05 +1000

    Implement Display trait for a list of vector

--- src/bin/formatted_print.rs
+++ src/bin/formatted_print.rs
@@ -20,6 +20,21 @@ impl fmt::Display for Complex {
     }
 }
 
+struct List(Vec<i32>);
+impl fmt::Display for List {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let vec = &self.0;
+        write!(f, "[")?;
+        for (count, v) in vec.iter().enumerate() {
+            if count != 0 {
+                write!(f, ", ")?;
+            }
+            write!(f, "{}: {}", count, v)?;
+        }
+        write!(f, "]")
+    }
+}
+
 fn main() {
     println!("{}", 32);
     println!("{1} {0}", 2, 3);
@@ -40,4 +55,7 @@ fn main() {
 
     println!("{:?}", Complex{ real: 3.3, imag: 7.2 });
     println!("{}", Complex{ real: 3.3, imag: 7.2 });
+
+    let v = List(vec![1, 2, 3]);
+    println!("{}", v);
 }
