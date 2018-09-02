de35777251d1a7edc8ea884c56746f0f052f5bb5
Qiang Ji <qiang.j@gmail.com>
2018-09-02 19:07:45 +1000

    Add String

--- src/bin/collections.rs
+++ src/bin/collections.rs
@@ -9,4 +9,18 @@ fn main() {
             println!("index is out of range");
         }
     }
+
+    // Create an empty string
+    let mut s = String::new();
+    s.push_str("Glenn");
+
+    let s1 = String::from("Glenn ");
+    let s2 = String::from("Ji");
+    let s3 = s1 + &s2;
+    println!("{}", s3);
+
+    let s4 = String::from("Joy ");
+    let s5 = String::from("Li");
+    let s6 = format!("{}{}", s4, s5);
+    println!("{}", s6);
 }
