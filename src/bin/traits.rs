cecff12c0ad0f59b97a7391795632011454c66f4
Qiang Ji <qiang.j@gmail.com>
2018-09-10 22:12:55 +1000

    Add trait

---
+++ src/bin/traits.rs
@@ -0,0 +1,49 @@
+trait Summary {
+    fn summarize(&self) -> String;
+}
+
+struct NewsArticle {
+    headline: String,
+    location: String,
+    author: String,
+    content: String
+}
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({}) - {}", self.headline, self.author, self.location, self.content)
+    }
+}
+
+struct Tweet {
+    username: String,
+    content: String,
+    reply: bool,
+    retweet: bool
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {} {} {}", self.username, self.content, self.reply, self.retweet)
+    }
+}
+
+fn main() {
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+
+    let news_article = NewsArticle {
+        headline: String::from("Melbourne is the best"),
+        location: String::from("Australia Victoria"),
+        author: String::from("Glenn ji"),
+        content: String::from("Melbourne is beautiful")
+    };
+
+    println!("{}", news_article.summarize());
+}
