b09946e648ddee9e7881780a0b9c14a96c34b7f8
Glenn Ji <glenn.ji@nab.com.au>
2018-09-13 09:31:29 +1000

    Conditionally implement methods on a generic type depending on trait bounds

--- src/bin/generic.rs
+++ src/bin/generic.rs
@@ -32,6 +32,30 @@ impl<T, U> Point<T, U> {
     }
 }
 
+struct Pair<T> {
+    x: T,
+    y: T
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self {
+            x,
+            y
+        }
+    }
+}
+
+impl<T: std::fmt::Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("x is greater than or equal to y");
+        } else {
+            println!("y is greater than x");
+        }
+    }
+}
+
 fn main() {
     let list = vec![3, 2, 4, 6, 1];
     let largest = largest(&list);
@@ -48,4 +72,7 @@ fn main() {
     let p3 = Point {x: 1, y: 2.0};
     println!("{:?}", p3);
     println!("{}", p3.x());
+
+    let pair = Pair::new(1, 2);
+    pair.cmp_display();
 }
