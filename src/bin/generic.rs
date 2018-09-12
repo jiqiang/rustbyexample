2473467f0515361560c1a2b8e51d9734b00a1830
Glenn Ji <glenn.ji@nab.com.au>
2018-09-13 09:21:13 +1000

    Remove Copy trait bound by returning reference of a slice item

--- src/bin/generic.rs
+++ src/bin/generic.rs
@@ -9,6 +9,17 @@ fn largest<T>(list: &[T]) -> T
     largest
 }
 
+fn largest2<T>(list: &[T]) -> &T
+    where T: PartialOrd {
+    let mut largest = &list[0];
+    for item in list.iter() {
+        if *item > *largest {
+            largest = item;
+        }
+    }
+    largest
+}
+
 #[derive(Debug)]
 struct Point<T, U> {
     x: T,
@@ -24,7 +35,9 @@ impl<T, U> Point<T, U> {
 fn main() {
     let list = vec![3, 2, 4, 6, 1];
     let largest = largest(&list);
-    println!("{}", largest);
+    println!("largest: {}", largest);
+    let largest2 = largest2(&list);
+    println!("largest2: {}", largest2);
 
     let p1 = Point {x: 1, y: 2};
     println!("{:?}", p1);
