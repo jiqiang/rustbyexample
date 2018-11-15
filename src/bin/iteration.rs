409d500496ab3fbd35ff2cc2db789439499a0116
Qiang Ji <qiang.j@gmail.com>
2018-11-15 22:04:19 +1100

    add iteration

--- src/bin/iteration.rs
+++ src/bin/iteration.rs
@@ -1,7 +1,32 @@
-fn main() {
-    let v = vec!( ("Herman".to_string(), 5));
-    let names: Vec<String> = v.into_iter().map(|(name, _score)| name).collect();
-    assert_eq!(names, ["Herman"]);
-    // into_iter() moves value so v is droped
+fn iterator1() {
+    let v = vec![1, 2, 3];
+    let mut v_iter = v.iter();
+    println!("{:?}", v_iter.next());
+    println!("{:?}", v);
+}
+
+fn iterator2() {
+    let v = vec![1, 2, 3];
+    let mut v_iter = v.into_iter();
+    println!("{:?}", v_iter.next());
     //println!("{:?}", v);
+}
+
+fn iterator3() {
+    let mut v = vec![1, 2, 3];
+    let mut v_iter = v.iter_mut();
+    match v_iter.next() {
+        Some(i) => {
+            *i += 1;
+        },
+        None => {
+            println!("{}", -1);
+        }
+    }
+}
+
+fn main() {
+    iterator1();
+    iterator2();
+    iterator3();
 }
