cccf041a33c2c8ee3bd7ff42262349f3e5ec5618
Qiang Ji <qiang.j@gmail.com>
2018-11-29 21:49:48 +1100

    implement deref trait

--- src/bin/boxes.rs
+++ src/bin/boxes.rs
@@ -6,7 +6,35 @@ enum List {
 
 use List::{Cons, Nil};
 
+use std::ops::Deref;
+
+struct MyBox<T>(T);
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+    
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
 fn main() {
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
     println!("{:?}", list);
+
+    let x = 5;
+    let y = Box::new(x);
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let x1 = 5;
+    let y1 = MyBox::new(x1);
+    assert_eq!(5, x1);
+    assert_eq!(5, *y1);
 }
