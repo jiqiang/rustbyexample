ca11f8c6d71519dea1586945803bdb6b143fcf97
Glenn Ji <glenn.ji@nab.com.au>
2018-12-24 10:37:43 +1100

    Using Trait Objects that Allow for Values of Different Types

---
+++ src/bin/gui.rs
@@ -0,0 +1,62 @@
+trait Draw {
+    fn draw(&self);
+}
+
+struct Screen {
+    components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+struct Button {
+    width: u32,
+    height: u32,
+    label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        println!("start drawing a button - {} {} {}", self.width, self.height, self.label);
+    }
+}
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        println!("start drawing a select box - {} {} {:?}", self.width, self.height, self.options);
+    }
+}
+
+fn main() {
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No")
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            })
+        ],
+    };
+
+    screen.run();
+}
