851b0485b049daa62fa1f17a5e9c06c505169e07
Qiang Ji <qiang.j@gmail.com>
2019-01-05 12:45:06 +1100

    writing a response

--- src/bin/web_server.rs
+++ src/bin/web_server.rs
@@ -14,5 +14,7 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 516];
     stream.read(&mut buffer).unwrap();
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let response = "HTTP/1.1 200 OK\r\n\rn";
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
