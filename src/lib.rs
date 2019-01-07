5ce6129cb2da63617059ac7ce5d7ef3824561877
Glenn Ji <glenn.ji@nab.com.au>
2019-01-07 11:26:45 +1100

    building the ThreadPool struct using compiler driven development

--- src/lib.rs
+++ src/lib.rs
@@ -58,3 +58,18 @@ mod tests {
         should_panic();
     }
 }
+
+pub struct ThreadPool;
+
+impl ThreadPool {
+    pub fn new(size: usize) -> ThreadPool {
+        ThreadPool
+    }
+
+    pub fn execute<F>(&self, f: F)
+        where
+            F: FnOnce() + Send + 'static
+    {
+
+    }
+}
